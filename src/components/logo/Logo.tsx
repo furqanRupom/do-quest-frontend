76e857612e1f685148b45d533f0b06381f8a1848
furqanrupom <furqanrupom978@gmail.com>
2026-02-05 21:50:56 +0600

    feat(logo): made logo linked

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import LogoIcon from './LogoIcon';
+import Link from 'next/link';
 
 
 
 const Logo: React.FunctionComponent = () => {
-  return <div className=' font-bold text-2xl inline-flex items-center '>
-    <LogoIcon />
+  return <Link href='/'>
+    <div className=' font-bold text-2xl inline-flex items-center '>
+      <LogoIcon />
       <h3 className='pt-3'>Do<span className='text-primary'>.</span><span>Quest</span></h3>
 
-  </div>;
+    </div>
+  </Link>;
 };
 
 export default Logo;
