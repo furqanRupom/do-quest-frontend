dab330d3ab1a3e5cf083f7bbd010e86869a13e9f
furqanrupom <furqanrupom978@gmail.com>
2026-02-11 22:39:51 +0600

    feat : added meta data to submissions page

--- src/app/(home)/submissions/page.tsx
+++ src/app/(home)/submissions/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Do.Quest | Submissions",
+    description: "View and manage your submissions for various quests on Do.Quest. Track your progress, receive feedback, and engage with the community to enhance your learning experience.",
+};
+
 const submissionsPage = () => {
-    return <h3>Submissioins page</h3>
+    return <h3>Submissions page</h3>
 }
 export default submissionsPage
