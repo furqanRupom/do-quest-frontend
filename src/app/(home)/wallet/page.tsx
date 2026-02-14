2e744551ed288e58b90a34e251ca40cc5d209218
furqanrupom <furqanrupom978@gmail.com>
2026-02-14 22:38:43 +0600

    feat: added do quest wallet page

--- src/app/(home)/wallet/page.tsx
+++ src/app/(home)/wallet/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title:"Do.Quest | Wallet",
+    description:"Do quests wallet page"
+};
 const walletpage = () => {
     return <h3>wallet page</h3>
 }
