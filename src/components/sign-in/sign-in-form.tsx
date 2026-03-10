e8991f773340ffa07688a8768933b82487346b52
furqanrupom <furqanrupom978@gmail.com>
2026-03-10 23:51:01 +0600

    chore: remove consoles

--- src/components/sign-in/sign-in-form.tsx
+++ src/components/sign-in/sign-in-form.tsx
@@ -15,7 +15,6 @@ import { loginUser } from "@/services/auth/loginUser";
 export default function SignInForm() {
     const [showPassword, setShowPassword] = useState(false);
     const [state, formAction, isPending] = useActionState(loginUser, null)
-    console.log(state);
     const getFieldError = (fieldName: string) => {
         if (!state?.errors) return null;
 
