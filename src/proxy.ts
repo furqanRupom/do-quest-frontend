da7561d3a1c59b4e984fc14a03c649e3150a696e
furqanrupom <furqanrupom978@gmail.com>
2026-03-20 23:27:40 +0600

    fixes

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,6 @@ import type { NextRequest } from 'next/server'
 
 export function proxy(request: NextRequest) {
     
-    console.log("From PROXY : ", request.cookies.get('accessToken')?.value)
     return NextResponse.redirect(new URL('/', request.url))
 }
 
