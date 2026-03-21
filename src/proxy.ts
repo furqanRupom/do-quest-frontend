fcf20780204cd2773a93c51153ce6feb5ccc28b1
furqanrupom <furqanrupom978@gmail.com>
2026-03-21 23:46:30 +0600

    fixes

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function proxy(request: NextRequest) {
-    
     return NextResponse.redirect(new URL('/', request.url))
 }
 
