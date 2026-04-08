a9beda04418d3de604c31ebdc4c42bdeab3d4bdc
furqanrupom <furqanrupom978@gmail.com>
2026-04-08 23:55:01 +0600

    chore : remove unnecessary code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
-
 
 const roboto = Roboto({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Do.Quest",
