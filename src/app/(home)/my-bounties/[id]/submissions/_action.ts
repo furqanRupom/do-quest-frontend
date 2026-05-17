e4edd5fa2234c2f4677f31fc4f6fcf9442dc5b8a
furqanrupom <furqanrupom978@gmail.com>
2026-05-17 21:51:40 +0600

    feat(view-submissions): include all apis in actions file

---
+++ src/app/(home)/my-bounties/[id]/submissions/_action.ts
@@ -0,0 +1,91 @@
+/* view submissions */
+"use server"
+
+import { getActionErrorMessage } from "@/lib/errorMessage"
+import { approveBountySubmission, rejectBountySubmission, revisionBountySubmission } from "@/services/submission.service"
+import { IRejectionSubmissionPayload, IRevisionSubmissionPayload, rejectBountySubmissionSchema, revisionBountySubmissionSchema } from "@/zod/submission.validation"
+
+export const approvedBountySubmissionAction = async (bountyId: string, submissionId: string) => {
+  if (!bountyId || !submissionId) {
+    return {
+      success: false,
+      message: "Invalid bounty or submission Id"
+    }
+  }
+
+  try {
+    return await approveBountySubmission(bountyId, submissionId)
+
+  } catch (error) {
+    console.error("Approved Submissions : ", error)
+    return {
+      success: false,
+      message: getActionErrorMessage(error, "Failed to approved submission")
+    }
+
+  }
+}
+
+
+
+
+export const rejectBountySubmissionAction = async (submissionId: string, payload: IRejectionSubmissionPayload) => {
+
+  if (!submissionId) {
+    return {
+      success: false,
+      message: "Invalid submission Id"
+    }
+  }
+  const parsedPayload = rejectBountySubmissionSchema.safeParse(payload)
+
+  if (!parsedPayload.success) {
+    return {
+      success: false,
+      message: parsedPayload.error.issues[0]?.message || "Invalid input",
+    }
+  }
+
+
+
+  try {
+    return await rejectBountySubmission(submissionId, parsedPayload.data)
+  } catch (error) {
+    console.error("Submission Rejection : ", error)
+    return {
+      succcess: false,
+      message: getActionErrorMessage(error, "Failed to reject submissions")
+    }
+  }
+
+}
+export const revisionBountySubmissionAction = async (submissionId: string, payload: IRevisionSubmissionPayload) => {
+
+  if (!submissionId) {
+    return {
+      success: false,
+      message: "Invalid submission Id"
+    }
+  }
+  const parsedPayload = revisionBountySubmissionSchema.safeParse(payload)
+
+  if (!parsedPayload.success) {
+    return {
+      success: false,
+      message: parsedPayload.error.issues[0]?.message || "Invalid input",
+    }
+  }
+
+
+
+  try {
+    return await revisionBountySubmission(submissionId, parsedPayload.data)
+  } catch (error) {
+    console.error("Submission Revision : ", error)
+    return {
+      succcess: false,
+      message: getActionErrorMessage(error, "Failed to revision submissions")
+    }
+  }
+
+}
